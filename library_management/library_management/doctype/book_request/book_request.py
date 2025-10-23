4efbf78d267d8a0e03602d7cf591eba084ff36d6
Sanjaa46 <sanjaas880@gmail.com>
2025-10-23 17:15:54 +0800

    fixing workflow

--- library_management/library_management/doctype/book_request/book_request.py
+++ library_management/library_management/doctype/book_request/book_request.py
@@ -6,7 +6,23 @@ from frappe.model.document import Document
 
 
 class BookRequest(Document):
-	def before_save(self):
-		user = frappe.session.user
-		library_member_name = frappe.get_value("Library Member", {"email_address": user}, "name")
-		self.library_member = library_member_name
+	
+	def on_update(self):
+		if not self.library_member:
+			user = frappe.session.user
+			library_member_name = frappe.get_value("Library Member", {"email_address": user}, "name")
+			self.library_member = library_member_name
+			self.save()
+
+		if self.status == "Approved":
+			library_member = self.library_member
+			book_name = self.article
+
+			transaction = frappe.get_doc({
+				"doctype": "Library Transaction",
+				"article": book_name,
+				"library_member": library_member,
+				"type": "Issue",
+				"date": frappe.utils.nowdate()
+			})
+			transaction.save()
--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,8 +1,8 @@
-frappe.ui.form.on('Library Member', {
-    refresh(frm) {
+// frappe.ui.form.on('Library Member', {
+//     refresh(frm) {
 
-        frm.add_custom_button('Create Transaction', () => {
-            frappe.new_doc('Library Transaction', { library_member: frm.doc.name });
-        });
-    }
-});
+//         frm.add_custom_button('Create Transaction', () => {
+//             frappe.new_doc('Library Transaction', { library_member: frm.doc.name });
+//         });
+//     }
+// });
