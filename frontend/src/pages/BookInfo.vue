6b8d6621eb3e853e911135fd2a589e2ba0ed5d60
Sanjaa46 <sanjaas880@gmail.com>
2025-11-06 17:19:29 +0800

    added issue book logic

--- frontend/src/pages/BookInfo.vue
+++ frontend/src/pages/BookInfo.vue
@@ -1,40 +1,41 @@
 <template>
 <Header></Header>
-
-<div class="w-[1120px] my-8 mx-auto text-[24px] font-bold relative">
-    <div class="absolute w-1 h-12 bg-[#118ab2] left-0"></div>
-    <h1 class="text-4xl font-bold pl-5 pt-2">Book Info</h1>
-</div>
-
-<section  class="flex w-[1120px] p-[30px] mx-auto my-10">
-    <!-- Book image and Author -->
-    <div class="flex flex-col w-[20%] items-center justify-center">
-        <img :src="book.image" class="w-[200px] h-[300px] object-contain rounded-[5px]" alt="book image">
-        <p class="text-center mt-5">{{ book.author }}</p>
+<div class="h-[810px] ">
+    <div class="w-[1120px] my-8 mx-auto text-[24px] font-bold relative">
+        <div class="absolute w-1 h-12 bg-[#118ab2] left-0"></div>
+        <h1 class="text-4xl font-bold pl-5 pt-2">Book Info</h1>
     </div>
 
-    <!-- Book information -->
-    <div class="w-[80%] ml-10">
-        <h1 class="text-[25px] font-medium">{{ book.article_name }}</h1>
-        <div class="flex my-5 space-x-5 text-[15px]">
-            <p><b>Author: </b>{{ book.author }}</p>
-            <p><b>Publisher: </b>{{ book.publisher }}</p>
-            <p><b>ISBN: </b>{{ book.isbn }}</p>
-            <p><b>Status: </b>{{ book.status }}</p>
+    <section  class="flex w-[1120px] p-[30px] mx-auto my-10">
+        <!-- Book image and Author -->
+        <div class="flex flex-col w-[20%] items-center justify-center">
+            <img :src="book.image" class="w-[200px] h-[300px] object-contain rounded-[5px]" alt="book image">
+            <p class="text-center mt-5">{{ book.author }}</p>
         </div>
-        <h1 class="text-[18px] font-medium">Description</h1>
-        <p>
-            {{ book.description }}
-        </p>
-        <Button 
-        @click="issueBook" 
-        variant="solid" 
-        :class='["bg-[#1290b9] w-[90px] text-white my-10 rounded hover:bg-[#016475] transition", issueButtonClasses]'
-        >
-        Issue
-        </Button>
-    </div>
-</section>
+
+        <!-- Book information -->
+        <div class="w-[80%] ml-10">
+            <h1 class="text-[25px] font-medium">{{ book.article_name }}</h1>
+            <div class="flex my-5 space-x-5 text-[15px]">
+                <p><b>Author: </b>{{ book.author }}</p>
+                <p><b>Publisher: </b>{{ book.publisher }}</p>
+                <p><b>ISBN: </b>{{ book.isbn }}</p>
+                <p><b>Status: </b>{{ book.status }}</p>
+            </div>
+            <h1 class="text-[18px] font-medium">Description</h1>
+            <p>
+                {{ book.description }}
+            </p>
+            <Button 
+            @click="issueBook" 
+            variant="solid" 
+            :class='["bg-[#1290b9] w-[90px] text-white my-10 rounded hover:bg-[#016475] transition", issueButtonClasses]'
+            >
+            Issue
+            </Button>
+        </div>
+    </section>
+</div>
 
 
 <Footer></Footer>
